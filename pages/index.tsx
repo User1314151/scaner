bfd54c24cb77823bf6dd4b72039fd426d4bea125
Tiago Silva <87677785+TSCTiago@users.noreply.github.com>
2023-11-08 09:38:06 -0300

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Home() {
   return (
     <div>
-      <h1>aqui é a Home</h1>
+      <h1>aqui é a Home mudado</h1>
       <div>
         <Link href="./qrScan">Scanner</Link>
       </div>
